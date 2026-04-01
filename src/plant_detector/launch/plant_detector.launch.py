1cdd5faacc7f7cd414e836d951991f382423fb23
zx <1158768027@qq.com>
2026-04-01 20:34:06 +0800

    update

--- src/plant_detector/launch/plant_detector.launch.py
+++ src/plant_detector/launch/plant_detector.launch.py
@@ -1,9 +1,9 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
 
 def generate_launch_description():
 
@@ -37,9 +37,23 @@ def generate_launch_description():
             # ("/livox/lidar", "/your/custom/topic"),
         ],
     )
+    # ── Include launch ─────────────────────────────────────────────
+    robot_base_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare("robot_base"),
+                "launch",
+                "agrobot_base_bringup.launch.py"
+            ])
+        ),
+        launch_arguments={
+            "use_ekf": "true",  # 启动 EKF
+        }.items(),
+    )
 
     return LaunchDescription([
         lidar_topic_arg,
         params_file_arg,
         detector_node,
+        robot_base_launch,
     ])
