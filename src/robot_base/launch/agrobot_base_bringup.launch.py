ec1d4fab460a359b930768f9d941b73aa593786c
zx <1158768027@qq.com>
2026-04-01 16:08:06 +0800

    update

--- src/robot_base/launch/agrobot_base_bringup.launch.py
+++ src/robot_base/launch/agrobot_base_bringup.launch.py
@@ -172,9 +172,9 @@ def generate_launch_description():
                 parameters=[
                     {
                         'target_frame': 'base_link',
-                        'min_x': -0.1,'max_x': 0.2,
-                        'min_y': -0.271,'max_y': 0.271,
-                        'min_z': -0.1,'max_z': 0.65,
+                        'min_x': -0.5,'max_x': 0.25,
+                        'min_y': -0.275,'max_y': 0.275,
+                        'min_z': -0.1,'max_z': 0.6,
                         # negative=True 表示删除盒子内部点
                         # False则只保留盒子内部点
                         'negative': True
