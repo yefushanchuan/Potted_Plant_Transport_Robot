e551b2bb7d03dff6c4354047a637d70be979cf43
zx <1158768027@qq.com>
2026-04-07 11:13:18 +0800

    update

--- src/robot_perception/pointcloud_to_laserscan/launch/pointcloud_to_laserscan_launch.py
+++ src/robot_perception/pointcloud_to_laserscan/launch/pointcloud_to_laserscan_launch.py
@@ -11,9 +11,23 @@ def generate_launch_description():
             description='Namespace for sample topics'
         ),
         Node(
-            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in',  ['/segmentation/obstacle']),
-                        ('scan',  ['/scan'])],
-            name='pointcloud_to_laserscan'
-        )
+            package='pointcloud_to_laserscan', 
+            executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', '/livox/lidar'),
+                        ('scan', '/scan')],
+            parameters=[{
+                'target_frame': 'base_footprint',  # 用地面中心作为基准
+                'transform_tolerance': 0.05,       # 稍微加大一点容忍度，防止TF延迟导致丢帧
+                'min_height': 0.15,                # 离地15cm以上
+                'max_height': 1.0,                 # 只要不超过车身高度，尽量设高点，弥补雷达倾斜带来的数据丢失
+                'angle_min': -3.14159,             # 小车底盘的正后方(右转180度)
+                'angle_max': 3.14159,              # 小车底盘的正后方(左转180度)
+                'angle_increment': 0.0087,         # 0.5度
+                'scan_time': 0.1,                  # 10Hz
+                'range_min': 0.2,                  # 滤掉车体自身
+                'range_max': 15.0,                 # 探测15米
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+        ),
     ])
